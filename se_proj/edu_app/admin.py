caa2fcd357b5df09d6cf5c0decc59e2e96aad0e0
setec2012 <setec2012@gmail.com>
2024-04-02 18:54:04 -0500

    add 6 tables

--- se_proj/edu_app/admin.py
+++ se_proj/edu_app/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
-
+from .models import Tbl_teacher,Tbl_student_teacher,Tbl_student,Tbl_student_class,Tbl_class,Tbl_assignment
 # Register your models here.
+
+#Tbl_class_admin
+class Tbl_class_admin(admin.ModelAdmin):
+    list_display = ('class_id', 'class_name', 'class_info','class_module','assignment_id','teacher_id')
+admin.site.register(Tbl_class,Tbl_class_admin)
+
+#Tbl_assignment_admin
+class Tbl_assignment_admin(admin.ModelAdmin):
+    list_display = ('assignment_id', 'assignment_name', 'assignment_percentage','assignment_due','class_id')
+admin.site.register(Tbl_assignment,Tbl_assignment_admin)
+
+#Tbl_teacher_admin
+class Tbl_teacher_admin(admin.ModelAdmin):
+    list_display = ('teacher_id', 'teacher_name', 'teacher_email')
+admin.site.register(Tbl_teacher,Tbl_teacher_admin)
+
+#Tbl_student_teacher_admin
+class Tbl_student_teacher_admin(admin.ModelAdmin):
+    list_display = ('id', 'student_id', 'teacher_id')
+admin.site.register(Tbl_student_teacher,Tbl_student_teacher_admin)
+
+#Tbl_student_admin
+class Tbl_student_admin(admin.ModelAdmin):
+    list_display = ( 'student_id', 'student_name','student_email')
+admin.site.register(Tbl_student,Tbl_student_admin)
+
+#Tbl_student_class_admin
+class Tbl_student_class_admin(admin.ModelAdmin):
+    list_display = ('id', 'student_id', 'class_id')
+admin.site.register(Tbl_student_class,Tbl_student_class_admin)
--- se_proj/edu_app/models.py
+++ se_proj/edu_app/models.py
@@ -14,3 +14,43 @@ class User(models.Model):
 class Test(models.Model):
     creator_id = models.IntegerField(primary_key=True)
     text = models.TextField()
+
+
+#Tbl_class
+class Tbl_class(models.Model):
+    class_id = models.AutoField(primary_key=True)  
+    class_name=models.CharField(max_length=200)
+    class_info=models.CharField(max_length=200)
+    class_module=models.CharField(max_length=200)
+    assignment_id=models.IntegerField()
+    teacher_id=models.IntegerField()
+
+#Tbl_assignment
+class Tbl_assignment(models.Model):
+    assignment_id = models.AutoField(primary_key=True)  
+    assignment_name=models.CharField(max_length=200)
+    assignment_percentage=models.CharField(max_length=200)
+    assignment_due=models.CharField(max_length=200)   
+    class_id=models.IntegerField()
+    
+#Tbl_teacher
+class Tbl_teacher(models.Model):
+    teacher_id = models.AutoField(primary_key=True)  
+    teacher_name=models.CharField(max_length=200)
+    teacher_email=models.EmailField(max_length=200)
+
+#Tbl_student_teacher
+class Tbl_student_teacher(models.Model):   
+    student_id=models.IntegerField()
+    teacher_id=models.IntegerField()
+
+#Tbl_student
+class Tbl_student(models.Model):
+    student_id = models.AutoField(primary_key=True)  
+    student_name=models.CharField(max_length=200)
+    student_email=models.EmailField(max_length=200)
+
+#Tbl_student_class
+class Tbl_student_class(models.Model):   
+    student_id=models.IntegerField()
+    class_id=models.IntegerField()
