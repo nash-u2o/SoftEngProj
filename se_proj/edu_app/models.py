9d1cec01fb46e6e1b0ba6d49d7bb992d2ee0ad09
Nash Euto <nash.euto20@gmail.com>
2024-04-03 15:47:28 -0500

    Added some comments

--- se_proj/edu_app/models.py
+++ se_proj/edu_app/models.py
@@ -17,15 +17,21 @@ class Test(models.Model):
 
 
 #Tbl_class
+"""
+* class_info should be a TextField
+* module should be a separate class
+* I don't see how foreign key for assignment_id does much
+"""
 class Tbl_class(models.Model):
     class_id = models.AutoField(primary_key=True)  
     class_name=models.CharField(max_length=200)
-    class_info=models.CharField(max_length=200)
-    class_module=models.CharField(max_length=200)
-    assignment_id=models.IntegerField()
+    class_info=models.CharField(max_length=200) 
+    class_module=models.CharField(max_length=200) 
+    assignment_id=models.IntegerField() 
     teacher_id=models.IntegerField()
 
 #Tbl_assignment
+
 class Tbl_assignment(models.Model):
     assignment_id = models.AutoField(primary_key=True)  
     assignment_name=models.CharField(max_length=200)
@@ -40,7 +46,10 @@ class Tbl_teacher(models.Model):
     teacher_email=models.EmailField(max_length=200)
 
 #Tbl_student_teacher
-class Tbl_student_teacher(models.Model):   
+"""
+* I don't know if this table is necessary
+"""
+class Tbl_student_teacher(models.Model):  #Don't know if this table is necessary 
     student_id=models.IntegerField()
     teacher_id=models.IntegerField()
 
