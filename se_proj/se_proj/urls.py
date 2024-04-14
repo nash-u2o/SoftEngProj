1bd9a32f042cf443e54b025a8215d224a8cf5b20
Nash Euto <nash.euto20@gmail.com>
2024-04-14 15:19:02 -0500

    Changed base to be more simplistic

--- se_proj/se_proj/urls.py
+++ se_proj/se_proj/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path("modules/", views.modules, name="modules"),
     path("home/", views.home, name="home"),
     path("info/", views.info, name="info"),
+    path("test/", views.test, name="test"),
 ]
